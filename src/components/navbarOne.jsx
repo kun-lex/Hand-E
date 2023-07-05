e12142be37918dc7a8f0177398bfe437297d9404
Dev.David <97886296+kun-lex@users.noreply.github.com>
2023-07-05 18:52:22 +0100

    update sign up page

---
+++ src/components/navbarOne.jsx
@@ -0,0 +1,101 @@
+import chevron from './icons/chevron.svg';
+import './App.css';
+import { Link } from "react-router-dom";
+import NewLogo from './images/Reba logo.png'
+
+export default function NavbarOne(){
+  function toggleMenu() {
+    document.body.classList.toggle("open");
+  };
+    return(
+      <div className='flex-col'>
+        <nav className="navbar">
+          <Link to='/'>
+          <div className="nav-title ml-[10px] object-contain"><img  src={NewLogo} alt="logo" /></div>
+          </Link>
+            
+          <div className="dropdowns">
+            <div className="dropdown">
+              <a className='flex items-center text-[#081E40] cursor-pointer mr-[10px] ' >
+              Auto Services
+              <img src={chevron}  alt="chevron"/>
+              </a>
+              <div className="dropdown-menu">
+                <ul>
+                  <li>Auto Repair</li>
+                  <li>Auto Detailing</li>
+                  <li>Car Wash</li>
+                  <li>Towing</li>
+                </ul>
+                <div>
+                  <button>Oil Change</button>
+                  <button>Car Dealer</button>
+                  <button>Parking</button>
+                  <button>Body Shop</button>
+                </div>
+              </div>
+            </div>
+            <div className="dropdown">
+              <a className='flex text-[#081E40] cursor-pointer mr-[10px] '>
+                Home Services
+                <img src={chevron}  alt="chevron" />
+              </a>
+              <div className="dropdown-menu">
+                <div>
+                  <button>Contractors</button>
+                  <button>Electricians</button>
+                  <button>Home Cleaners</button>
+                  <button>Plumbers</button>
+                </div>
+                <div>
+                  <button>Movers</button>
+                  <button>Painters & <br/> Decorators</button>
+                  <button>LockSmiths</button>
+                  <button>HVAC</button>
+                </div>
+              </div>
+            </div>
+            <div className="dropdown">
+              <a className='flex text-[#081E40] cursor-pointer mr-[10px] '>
+                Reba For Business
+                <img src={chevron}  alt="chevron" />
+              </a>
+              <div class="dropdown-menu">
+                <div>
+                  <button>Add a Business</button>
+                  <button>Claim your business</button>
+                  <button>Log in to Business Account</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Link to="/login">
+            <a className='flex text-[#081E40] cursor-pointer '
+              style={{
+                width : '50px',
+                height: '30px',
+                color:'#081E40',
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}
+            >Log In</a>
+          </Link>
+          <Link>
+            <a className='flex items-center cursor-pointer mr-[10px] '
+              style={{
+                width : '70px',
+                height: '30px',
+                border:'1px solid #fff',
+                background:'#1D5EBF',
+                color:'white',
+                borderRadius:'19.5px',
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}
+            >Sign Up</a>
+          </Link>
+          <button onClick={toggleMenu} className="burger"> </button>
+        </nav>
+      </div>
+    )
+}
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,8 +1,10 @@
 import '../components/App.css'
 // import Log from '../components/icons/log.svg';
-import Navbar from '../components/Navbar'
+import NavbarOne from '../components/navbarOne';
 import loginimg from '../components/icons/undraw_login_re_4vu2.svg'
 import { styled } from 'styled-components';
+import Footer from '../components/Footer';
+import GoogleIcon from '../components/images/icons8-google-48.png'
 
 export default function Login(){
     const WrapStyle = styled.div`
@@ -23,15 +25,16 @@ export default function Login(){
     `;
     return(
         <div>
-            <Navbar />
-            <div className='flex items-center justify-center mt-[100px]'>
+            <NavbarOne />
+            <div className='flex items-center justify-center w-full mt-[50px]'>
                 <div className='flex'>
                     <div className='flex flex-col items-center'>
                         <h1 style={{
                             color:"#081E40",
                             fontFamily : 'Sanchez',
                             fontSize : '32px',
-                            lineHeight : '41px'
+                            lineHeight : '41px',
+                            marginBottom: '20px'
                             }}>
                             Sign In
                         </h1>
@@ -40,7 +43,9 @@ export default function Login(){
                                 background : '#fffff',
                                 borderRadius : '20px',
                                 width : '250px',
-                                height : '50px'
+                                height : '50px',
+                                padding: '10px',
+                                marginBottom: '20px'
                             }}
                             type="text"
                             placeholder='Username or Email'
@@ -50,7 +55,9 @@ export default function Login(){
                                 background : '#fffff',
                                 borderRadius : '20px',
                                 width : '250px',
-                                height : '50px'
+                                height : '50px',
+                                padding: '10px',
+                                marginBottom: '20px'
                             }}
                             type="password"
                             placeholder='Password'
@@ -58,31 +65,52 @@ export default function Login(){
                         <button
                             style={{
                                 display : 'flex',
-                                width : '106px',
-                                height : '36px',
+                                width : '70px',
+                                height : '25px',
                                 borderRadius : '5px',
                                 background :'#1352B0',
                                 color : 'white',
                                 alignItems : 'center',
+                                justifyContent : 'center',
                                 textAlign : 'center',
-                                fontFamily : 'Forum'
+                                fontFamily : 'Forum',
                             }}
                         >Sign In</button>
-                        <div className='flex w-auto items-center'>
-                            <div className='w-[70px] h-[1px] bg-zinc-700'></div>
-                            <h5>or</h5>
-                            <div className='w-[70px] h-[1px] bg-zinc-700'></div>
+                        <a className='cursor-pointer' >Forgot Password ?</a>
+                        <div className='flex items-center '>
+                            <h5 className=' p-[5px] ' >or</h5>
                         </div>
                         <button
                             style={{
-                                width: '352px',
-                                height: '62px',
+                                width: '200px',
+                                height: '40px',
                                 borderRadius: '32px',
-                                background: '#000',
-                            }}  
-                        >Sign in with Google</button>
+                                background: 'black',
+                                color: 'white',
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                marginBottom: '10px'
+                            }}
+                        >Continue with Google <img src={GoogleIcon} className=' w-[10px] h-[10px] ' alt='google' /> </button>
+                        <p className='text-black' >Don't have an Account ?</p>
+                        <button
+                            style={{
+                                display : 'flex',
+                                width : '70px',
+                                height : '25px',
+                                borderRadius : '5px',
+                                background :'#1352B0',
+                                color : 'white',
+                                alignItems : 'center',
+                                justifyContent : 'center',
+                                textAlign : 'center',
+                                fontFamily : 'Forum',
+                                marginBottom: '20px'
+                            }}
+                        >Sign Up</button>
                     </div>
-                    <div className='w-[1px] h-[580px] bg-zinc-700'></div>
+                    <div className='w-[1px] h-[200px] bg-zinc-700'></div>
                     <img
                     style={{
                         width:'500px',
@@ -91,6 +119,7 @@ export default function Login(){
                     src={loginimg} alt='log in' />
                 </div>
             </div>
+            <Footer/>
         </div>
     )
 }
