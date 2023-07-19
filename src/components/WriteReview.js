e54afe908f30d8f8514a1c3de859a2a7a25bb397
Dev.David <97886296+kun-lex@users.noreply.github.com>
2023-07-19 21:23:44 +0100

    update write review section

--- src/components/WriteReview.js
+++ src/components/WriteReview.js
@@ -1,16 +1,21 @@
-import Navbar from './Navbar'
 import SelectCategory from './SelectCategory';
 import  ReviewLogo from './icons/undraw_for_review_eqxk.svg';
+import NavbarOne from './navbarOne';
 
 const WriteReview = () => {
     return(
         <div>
-            <Navbar/>
-            <div className='flex'>
-                <SelectCategory/>
+            {/* navbar would onl contain user avatar and logo */}
+            <NavbarOne/>
+            <h1 className=' text-black '> Select the business you'd like to review </h1>
+            <h2>Review anything from your favorite patio spot to your local flower shop.</h2>
+            <div className='flex items-center'>
+                <div className=' w-11/12 sm:w-[60%] mx-auto '>
+                    <SelectCategory/>
+                </div>
                 <img style={{ width: "150px", height: "150px", justifyContent: "flex-end", display: "flex", marginRight: "auto" }} src={ReviewLogo} alt='review logo' /> 
             </div>
-            <h2> Select the business you'd like to review </h2>
+            
 
         </div>
     )
